b8d227fb990a75d9014e4f5055c2d70b8a7aa678
widiskel <waputra16@gmail.com>
2025-01-31 11:09:04 +0700

    update

--- config/config_tmp.js
+++ config/config_tmp.js
@@ -1,11 +1,15 @@
 export class Config {
   static TXAMOUNTMIN = 0.1; //TX AMOUNT MIN
   static TXAMOUNTMAX = 0.5; //TX AMOUNT MAX
-  static SWAPCOUNT = 10; //SWAP COUNT
-  static STAKENODEOPERATOR =
-    "0xcf4b9402e7f156bc75082bc07581b0829f081ccfc8c444c71df4536ea33d094a"; //Operator: Mysten Labs 0
+  static SWAPCOUNT = 5; //SWAP COUNT
+  static STAKENODEOPERATOR = [
+    "0x001e1696af97d14ceefdf5771c0ca74f5375d9b0ae57ccdb2b9d8ce212762b6e", //Operator: BartestneT
+    "0x015e3daef941d0d87de2c586bebacbde4c8a29c60f09d8e8eb854c5719c002d1", //Operator: Graphyte
+    "0x01ef8dad1ea8afe318fe2003a5123b20c4fe6511659ecf46d968851c15b67134", //Operator: LakeStake
+  ];
+
   static DISPLAY = "BLESS"; // TWIST OR BLESS
-  static DELAYINHOURS = 24;
+  static DELAYINHOURS = 6;
 
   //NETWORK
   static RPC = {
